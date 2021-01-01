d12d1dec4c5f1e00946439728d667ea579e53241
jkres77 <jkresweber@gmail.com>
2021-01-01 17:03:02 +0000

    *removed 'lhs == 0' from if statements *removed else bracket from line 195

--- main.cpp
+++ main.cpp
@@ -118,7 +118,7 @@ float FloatType::multiply(float lhs, float rhs)
 
 float FloatType::divide(float lhs, float rhs)
 {
-    if (lhs == 0.0f || rhs == 0.0f)
+    if (rhs == 0.0f)
     {
         std::cout << "\nwarning, floating point division by zero returns 'inf' !" << std::endl;
     }
@@ -152,7 +152,7 @@ double DoubleType::multiply(double lhs, double rhs)
 
 double DoubleType::divide(double lhs, double rhs)
 {
-    if (lhs == 0.0 || rhs == 0.0)
+    if (rhs == 0.0)
     {
         std::cout << "\nwarning, floating point division by zero returns 'inf' !" << std::endl;
     }
@@ -186,17 +186,13 @@ int IntType::multiply(int lhs, int rhs)
 
 int IntType::divide(int lhs, int rhs)
 {
-    if (lhs == 0 || rhs == 0)
+    if (rhs == 0)
     {
         std::cout << "error, integer division by zero will crash the program!\n";
         std::cout << "returning lhs" << std::endl;
         return lhs;
     }
-    else 
-    {
-        return lhs / rhs;
-    }
-    
+    return lhs / rhs;
 }
 
 //=======================================================
